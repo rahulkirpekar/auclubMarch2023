0886ac52313f0450dda4f9e6abe65f2c2f60afae
rahul kirpekar <rahul.kirpekar@gmail.com>
2023-04-03 21:47:52 +0530

    io

---
+++ src/iotopic/sertopic/TestApp3.java
@@ -0,0 +1,40 @@
+package iotopic.sertopic;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class TestApp3 
+{
+	public static void main(String[] args) 
+	{
+		Student s[] = new Student[5];
+		for (int i = 0; i < s.length; i++) 
+		{
+			s[i] = new Student();
+			s[i].scan();
+		}
+		
+		try (
+				FileOutputStream fout = new FileOutputStream("test2.txt");
+				ObjectOutputStream out = new ObjectOutputStream(fout);	
+			)
+		{
+		
+			out.writeObject(s);
+
+			System.out.println("success");
+		} catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+}
